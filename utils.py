812a33f28b416f111dbc13d7444b4983c53a2991
Leonardo Russo <leonardo.rxsso@gmail.com>
2024-04-10 17:09:38 +0200

    version 1.0

--- utils.py
+++ utils.py
@@ -18,21 +18,30 @@ def save_model(model, path):
     torch.save(model.state_dict(), path)
 
 
-def visualize_reconstruction(original, reconstructed, epoch, idx, device):
+def visualize_reconstruction(original, reconstructed, epoch):
     # Assuming original and reconstructed are tensors of shape (B, C, H, W)
     fig, axs = plt.subplots(1, 2, figsize=(16, 8))
     original = make_grid((original).cpu(), nrow=6)
     reconstructed = make_grid((reconstructed).cpu(), nrow=6)
 
     axs[0].imshow(original.permute(1, 2, 0))
-    axs[0].set_title('Original Images')
+    axs[0].set_title(f'Epoch: {epoch+1} - Original Images')
     axs[0].axis('off')
 
     axs[1].imshow(reconstructed.permute(1, 2, 0))
-    axs[1].set_title('Reconstructed Images')
+    axs[1].set_title(f'Epoch: {epoch+1} - Reconstructed Images')
     axs[1].axis('off')
 
     plt.show()
+
+
+def combined_loss(output, target, alpha=1, beta=1):
+
+    huber_loss = nn.HuberLoss()
+    perceptual_loss = PerceptualLoss()
+
+    return alpha * huber_loss(output, target) + beta * perceptual_loss(output, target)
+
 
 
 class PerceptualLoss(nn.Module):
